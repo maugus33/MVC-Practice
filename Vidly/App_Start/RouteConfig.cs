53d2b221a96a4b493606146f996376aa3fe0f3b2
Malissa A <malissa.augustin@gmail.com>
2016-09-24 14:23:56 -0400

    Initial commit  - added a list of customers and movies - practice mvc

---
+++ Vidly/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Vidly
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapMvcAttributeRoutes();
+
+            /*below is a custom route *OLD WAY OF DOING THINGS. TO COUPLED DUE TO MAGIC STRINGS. USE ATTRIBUTE ROUTING INSTEAD */
+            /*routes.MapRoute(
+                name: "MoviesByReleaseDate",
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate" },
+                constraints: new { year = @"\2015|2016", month = @"\d{2}"}// @ makes it a string since "\" is an escape character. add regular expressions for constraints
+                );*/
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
